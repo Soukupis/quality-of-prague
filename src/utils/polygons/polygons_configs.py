a30b084ded91cd3fec1a8555cd4d8bbb35a43ed8
Joseph Soukup <joseph.soukup@outlook.com>
2025-12-14 16:51:04 +0100

    Change colors for polygons and scatters

--- src/utils/polygons/polygons_configs.py
+++ src/utils/polygons/polygons_configs.py
@@ -1,24 +1,24 @@
 POLYGON_LAYERS_CONFIGS = {
     "no_standing": {
-        "background_color": "red",
+        "background_color": "#DC143C",
         "key": "no_standing",
         "name": "Zákaz stání",
         "legend_group": "Cestování",
     },
     "loading_zone": {
-        "background_color": "lightblue",
+        "background_color": "#FF8C00",
         "key": "loading_zone",
         "name": "Zásobování",
         "legend_group": "Cestování",
     },
     "designated_parking": {
-        "background_color": "lightgreen",
+        "background_color": "#9C27B0",
         "key": "designated_parking",
         "name": "Stání speciální",
         "legend_group": "Cestování",
     },
     "paid_parking": {
-        "background_color": "blue",
+        "background_color": "#1976D2",
         "key": "paid_parking",
         "name": "Placené stání",
         "legend_group": "Cestování",
